05a553b2919766499ad3ac58b3dc6c25e47abcef
Zeeshan-Siddiqui123 <155655770+Zeeshan-Siddiqui123@users.noreply.github.com>
2024-01-25 04:04:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let age = ("Age: 17");
+console.log(age);
+
+let student = ("I am a Student");
+console.log(student);
+
+alert("Wellcome to my Webpage");
+alert("Assalamwalikum Hi I am Zeeshan Ahmed Siddiqui I am the student of first year and i want to become a software engineer ");
+
+
+let area = ("Area of Rectangle");
+console.log(area);
+
+let length = ("Lenth = 45");
+console.log(length);
+
+let Width = ("Width = 85");
+console.log(Width);
+
+let h = ("Area Of Rectangle = ?")
+console.log(h)
+
+let Solution = ("Solution");
+console.log(Solution);
+
+let Area = ("Area of Rectangle = L*W");
+console.log(Area);
+
+let multiply = ("Area of Rectangle = 45 X 85");
+console.log(multiply);
+
+
+let a = (45);
+let b = (85);
+console.log("Area of Rectangle = ", a*b , "sq.cm");
+
+let i = 1
+while (i<=10) {
+     let result = 3*i
+     console.log("3 X ", i, "=", result)
+     i++
+}
+
+ prompt("What Is Your Name ?")
+ alert("Hello! How Are you What Are You Doing Today?")
+
+ let userName = ("I am Fine what about you")
+ console.log(userName)
